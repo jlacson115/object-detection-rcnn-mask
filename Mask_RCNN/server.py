2c49794abf9b81de026edcee8751b9bbc84d9d72
Jessica M. Lacson <jlacson@elementsolutions.com>
2019-11-06 09:32:11 -0500

    added CORS

--- Mask_RCNN/server.py
+++ Mask_RCNN/server.py
@@ -1,11 +1,17 @@
 from flask import Flask, request, jsonify, send_file
+from flask.ext.cors import CORS, cross_origin
 from PIL import Image
 import io
 import predict
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy   dog'
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+cors = CORS(app, resources={r"/predict": {"origins": "*"}})
 
 @app.route('/predict', methods=['POST'])
+@cross_origin(origin='*',headers=['Content-Type','Authorization'])
 def run():
     data = request.get_json(force=True)
     input_params = data['input']
@@ -31,3 +37,4 @@ def hi():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
+    
